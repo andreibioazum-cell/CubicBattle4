f4a2f7c4f827aa1c0103316dab1441811d4abe7f
andreibioazum-cell <andreibioazum@gmail.com>
2026-07-31 14:46:46 +0400

    Update runtime.c

--- runtime.c
+++ runtime.c
@@ -1,57 +1,62 @@
-#ifndef RUNTIME_H
-#define RUNTIME_H
+#include "runtime.h"
 
-#include <stdint.h>
-#include <android/asset_manager.h>
-#include <android/native_window.h>
-#include <android/log.h>
-#include <math.h>
-#include <stdlib.h>
-#include <string.h>
+Table* G = NULL;
+Table* L = NULL;
+Joy joy = {0};
+int screen_w = 0, screen_h = 0;
+double fps = 0;
 
-typedef struct { uint32_t* pixels; int width, height, stride; } Buffer;
-typedef struct { float x, y; } Vec2;
-typedef struct { float x, y, z; } Vec3;
+static uint32_t hash(const char* s) {
+    uint32_t h = 0;
+    while (*s) h = h * 31 + *s++;
+    return h;
+}
 
-#define V2(x,y) (Vec2){x,y}
-#define V2A(a,b) (Vec2){a.x+b.x, a.y+b.y}
-#define V2S(a,b) (Vec2){a.x-b.x, a.y-b.y}
-#define V2M(a,s) (Vec2){a.x*s, a.y*s}
-#define V2L(a) sqrtf(a.x*a.x + a.y*a.y)
+Table* T_new() { return calloc(1, sizeof(Table)); }
 
-typedef struct Entry { struct Entry* next; uint32_t hash; char* key; void* val; int type; } Entry;
-typedef struct { Entry* buckets[128]; int count; } Table;
+void T_set(Table* t, const char* key, void* val, int type) {
+    uint32_t h = hash(key) % 128;
+    Entry* e = t->buckets[h];
+    while (e) { if (strcmp(e->key, key)==0) { e->val=val; e->type=type; return; } e=e->next; }
+    e = malloc(sizeof(Entry));
+    e->key = strdup(key);
+    e->hash = hash(key);
+    e->val = val;
+    e->type = type;
+    e->next = t->buckets[h];
+    t->buckets[h] = e;
+    t->count++;
+}
 
-typedef struct { int type; union { double num; char* str; Table* table; void* func; Vec2 v2; Vec3 v3; }; } Val;
+void* T_get(Table* t, const char* key, int* type) {
+    uint32_t h = hash(key) % 128;
+    Entry* e = t->buckets[h];
+    while (e) { if (strcmp(e->key, key)==0) { if(type)*type=e->type; return e->val; } e=e->next; }
+    if(type) *type = 0;
+    return NULL;
+}
 
-extern Table* G;
-extern Table* L;
-extern int screen_w, screen_h;
-extern double fps;
+void print(Val v) {
+    switch(v.type) {
+        case 1: __android_log_print(ANDROID_LOG_INFO, "DS", "%f", v.num); break;
+        case 2: __android_log_print(ANDROID_LOG_INFO, "DS", "%s", v.str); break;
+        default: __android_log_print(ANDROID_LOG_INFO, "DS", "nil"); break;
+    }
+}
 
-typedef struct { float x, y, dx, dy, ox, oy, r; } Joy;
-extern Joy joy;
+void printn(double n) { __android_log_print(ANDROID_LOG_INFO, "DS", "%f", n); }
+void prints(const char* s) { __android_log_print(ANDROID_LOG_INFO, "DS", "%s", s); }
+double tonumber(Val v) { return v.type==1 ? v.num : 0; }
+const char* tostring(Val v) { return v.type==2 ? v.str : ""; }
 
-Table* T_new(void);
-void T_set(Table* t, const char* key, void* val, int type);
-void* T_get(Table* t, const char* key, int* type);
+void cls(uint32_t color) {}
+void rect(float x, float y, float w, float h, uint32_t color) {}
+void circle(float x, float y, float r, uint32_t color) {}
+void ring(float x, float y, float r, float t, uint32_t color) {}
+void tex(float x, float y, const char* name, float angle, float scale) {}
+void text(const char* str, float x, float y, uint32_t color) {}
 
-void print(Val v);
-void printn(double n);
-void prints(const char* s);
-double tonumber(Val v);
-const char* tostring(Val v);
-
-void cls(uint32_t color);
-void rect(float x, float y, float w, float h, uint32_t color);
-void circle(float x, float y, float r, uint32_t color);
-void ring(float x, float y, float r, float t, uint32_t color);
-void tex(float x, float y, const char* name, float angle, float scale);
-void text(const char* str, float x, float y, uint32_t color);
-
-void init(AAssetManager* assets);
-void update(void);
-void draw(Buffer* rb);
-void touch(float x, float y, int action);
-
-#endif
+void init(AAssetManager* assets) { G = T_new(); }
+void update() {}
+void draw(Buffer* rb) {}
+void touch(float x, float y, int action) {}
