319b5aeb0df357f543cfdf04f5e32dc7eeddad52
andreibioazum-cell <andreibioazum@gmail.com>
2026-07-31 14:57:22 +0400

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Супер-быстрый генератор game.c из game.ds
 
 cat > game.c << 'EOF'
 #include "runtime.h"
@@ -9,17 +8,17 @@ Table* L = NULL;
 
 void init(AAssetManager* assets) {
     G = T_new();
-    T_set(G, "COLOR_BG", &(Val){1,.num=0xFFCCCCCC}, 1);
-    T_set(G, "COLOR_PLAYER", &(Val){1,.num=0xFFEE7722}, 1);
-    T_set(G, "COLOR_JOY", &(Val){1,.num=0xFF000000}, 1);
-    T_set(G, "COLOR_TEXT", &(Val){1,.num=0xFFFFFFFF}, 1);
+    T_set(G, "COLOR_BG", &(Val){.type=1,.num=0xFFCCCCCC}, 1);
+    T_set(G, "COLOR_PLAYER", &(Val){.type=1,.num=0xFFEE7722}, 1);
+    T_set(G, "COLOR_JOY", &(Val){.type=1,.num=0xFF000000}, 1);
+    T_set(G, "COLOR_TEXT", &(Val){.type=1,.num=0xFFFFFFFF}, 1);
     
     Table* p = T_new();
-    T_set(p, "x", &(Val){1,.num=screen_w/2.0}, 1);
-    T_set(p, "y", &(Val){1,.num=screen_h/2.0}, 1);
-    T_set(p, "angle", &(Val){1,.num=0}, 1);
-    T_set(p, "speed", &(Val){1,.num=5}, 1);
-    T_set(p, "size", &(Val){1,.num=40}, 1);
+    T_set(p, "x", &(Val){.type=1,.num=screen_w/2.0}, 1);
+    T_set(p, "y", &(Val){.type=1,.num=screen_h/2.0}, 1);
+    T_set(p, "angle", &(Val){.type=1,.num=0}, 1);
+    T_set(p, "speed", &(Val){.type=1,.num=5}, 1);
+    T_set(p, "size", &(Val){.type=1,.num=40}, 1);
     T_set(G, "player", p, 3);
     
     joy.x = 150;
@@ -29,7 +28,7 @@ void init(AAssetManager* assets) {
 
 void update() {
     Val* pv = T_get(G, "player", NULL);
-    Table* p = pv ? pv->as.table : NULL;
+    Table* p = pv ? (Table*)pv->table : NULL;
     if (!p) return;
     
     Val* x = T_get(p, "x", NULL);
@@ -53,8 +52,9 @@ void update() {
 
 void draw(Buffer* rb) {
     cls(0xFFCCCCCC);
+    
     Val* pv = T_get(G, "player", NULL);
-    Table* p = pv ? pv->as.table : NULL;
+    Table* p = pv ? (Table*)pv->table : NULL;
     if (p) {
         Val* x = T_get(p, "x", NULL);
         Val* y = T_get(p, "y", NULL);
@@ -64,6 +64,7 @@ void draw(Buffer* rb) {
             rect(x->num - h, y->num - h, sz->num, sz->num, 0xFFEE7722);
         }
     }
+    
     ring(joy.x, joy.y, joy.r, 4, 0xFF000000);
     circle(joy.x + joy.ox, joy.y + joy.oy, 35, 0xFF000000);
     text("FPS: 60", 10, 10, 0xFFFFFFFF);
@@ -73,9 +74,20 @@ void touch(float x, float y, int action) {
     float dx = x - joy.x;
     float dy = y - joy.y;
     float dist = sqrt(dx*dx + dy*dy);
-    if (action == 1) { joy.dx=0; joy.dy=0; joy.ox=0; joy.oy=0; return; }
+    
+    if (action == 1) {
+        joy.dx = 0; joy.dy = 0;
+        joy.ox = 0; joy.oy = 0;
+        return;
+    }
+    
     if (dist > joy.r + 30) return;
-    if (dist < 15) { joy.dx=0; joy.dy=0; joy.ox=0; joy.oy=0; return; }
+    if (dist < 15) {
+        joy.dx = 0; joy.dy = 0;
+        joy.ox = 0; joy.oy = 0;
+        return;
+    }
+    
     float c = dist > joy.r ? joy.r : dist;
     joy.dx = dx / dist;
     joy.dy = dy / dist;
@@ -84,4 +96,4 @@ void touch(float x, float y, int action) {
 }
 EOF
 
-echo "game.c generated in 0.01s"
+echo "game.c generated!"
