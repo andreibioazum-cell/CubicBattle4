e58ff6b17cba088606977933ad8c719565ede823
andreibioazum-cell <andreibioazum@gmail.com>
2026-08-06 23:14:02 +0400

    Update gg.py

--- gg.py
+++ gg.py
@@ -1,4 +1,5 @@
 import random
-def generate_random_number():
-return random.randint(1, 100)
-print(f"Random number: {generate_random_number()}")
+def main():
+print(f"Random number: {random.randint(1, 100)}")
+if __name__ == "__main__":
+main()
